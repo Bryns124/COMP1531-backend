3a983ca0a4b19afa6b8e29470a75b9b77f471e14
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-02 21:32:03 +1100

    Making revisions on the fixtures i have created, and attempting to access a dictionary in a dictionary

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,5 +1,5 @@
 from src.channel import channel_messages_v1, channel_invite_v1, channel_details_v1
-from src.channels import channels_create_v1
+from src.channels import channels_create_v1, channels_list_v1
 from src.auth import auth_register_v1, auth_login_v1
 from src.error import AccessError, InputError 
 from src.other import clear_v1
@@ -11,20 +11,19 @@ def user_1():
 @pytest.fixture 
 def user_2():
     return auth_register_v1("miguel@unsw.com", "test", "Miguel", "Test")
-
 @pytest.fixture
-def channel_1():
+def channel_1(user_1):
     return channels_create_v1(user_1["auth_user_id"], "Test Channel", True)
    
-def test_channel_invite_access_error(user_1, channel_1, user_2):    
-    with pytest.raises(AccessError):
-        assert user_1['u_id']
-    clear_v1()
+# def test_channel_invite_access_error(user_1, channel_1, user_2):    
+#     with pytest.raises(AccessError):
+#         assert user_1['u_id']
+#     clear_v1()
         
-def test_channel_invite_input_error(user_1, channe_1, user_2):
-    with pytest.raises(InputError):
-        assert
-    clear_v1()
+# def test_channel_invite_input_error(user_1, channe_1, user_2):
+#     with pytest.raises(InputError):
+#     assert 
+#     clear_v1()
 def test_channel_invite(user_1, channel_1, user_2):
     """
     :auth_user_id: the u_id of the user executing the command
@@ -32,7 +31,8 @@ def test_channel_invite(user_1, channel_1, user_2):
     :param: u_id: the u_id of the person receiving the invite 
     return: we return nothing for this iteration, the user is just added straight away. 
     """
-    assert channel_details_v1(channel_1['channel_id'])
+    channel_invite_v1(user_1['auth_user_id'], channel_1['channel_id'], user_2['auth_user_id'])
+    assert channels_list_v1(user_2['auth_user_id']) == channel_1['channel_id'] 
     #however need to test that the user was added sucessfully so need to check the channel details and find the user in the list of users in the channel 
     clear_v1()
     
