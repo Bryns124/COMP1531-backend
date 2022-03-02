415e3160d1d4a8ce02461231015884e7008a8c6d
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-02 11:28:12 +1100

    Wrote the stub code for channels_create

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+import data_store
+import channel
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +22,18 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
-    return {
-        'channel_id': 1,
-    }
+    store = data_store.get()
+    store['channels']['name'].append(name)
+    store['channels']['is_public'].append(is_public)
+    
+    channel_id = len(store['channels'][channel_id]) - 1
+    store['channels']['channel_id'].append(channel_id)
+    store['channels']['channel_owner'].append(auth_user_id)
+    store['channels']['channel_members'].append([auth_user_id])
+    store['channels']['channel_messages'].append([])
+    
+    data_store.set(store)
+    
+    channel_join_v1(auth_user_id, channel_id)
+    
+    return channel_id
