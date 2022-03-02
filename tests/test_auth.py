271e911ce22f24fde22cce0f3e8c3ad70c14d4c3
z5361001 <z5361001@ad.unsw.edu.au>
2022-03-02 16:54:50 +1100

    moved clear_v1 to bottom of test functions for login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,7 +7,6 @@ from src.other import clear_v1
 # LOGINS
 # test when login email is invalid
 def test_login_invalid_email():
-    clear_v1()
     with pytest.raises(InputError):
         auth_login_v1("bryanle", "password123")
         auth_login_v1("bryanle@gmailcom", "password123")
@@ -18,17 +17,18 @@ def test_login_invalid_email():
         auth_login_v1("bryanle-@gmail.com", "password123")
         auth_login_v1("@gmail", "password123")
         auth_login_v1("", "password123")
+    clear_v1()
         
 # test when login email is incorrect
 def test_login_incorrect_email():
-    clear_v1()
     auth_register_v1("bryan.le@gmailcom", "password123", "Bryan", "Le")
     with pytest.raises(InputError):
         assert auth_login_v1("notbryan.le@gmail.com", "password123") == {'auth_user_id': 1}
+    clear_v1()
         
 # test when login password is incorrect
 def test_login_incorrect_password():
-    clear_v1()
     auth_register_v1("bryan.le@gmailcom", "password123", "Bryan", "Le")
     with pytest.raises(InputError):
         assert auth_login_v1("bryan.le@gmailcom", "password456") == {'auth_user_id': 1}
+    clear_v1()
