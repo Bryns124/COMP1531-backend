5ca96f44817f686c8fd372579aa6902bf2aa26b5
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-02 13:34:59 +1100

    Made some progress on fixtures and added a linter file

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,2 +1,18 @@
 from src.channel import channel_messages_v1, channel_invite_v1
+from src.channels import channels_create_v1
+from src.auth import auth_register_v1
 
+@fixture
+def user_1():
+    return auth_register_v1("mikey@unsw.com", "test", "Mikey", "Test")
+def user_2():
+    return auth_register_v1("miguel@unsw.com", "test", "Miguel", "Test")
+
+@fixture
+def channel_1():
+    return channels_create_v1(user_1["auth_user_id"], "Test Channel", True)
+    
+    
+    
+
+    
