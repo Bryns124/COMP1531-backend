931255ec994ce89fd8892414e9e20dc5764e671d
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-02 14:27:59 +1100

    formated code

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -5,9 +5,9 @@ from src.auth import auth_register_v1
 @fixture
 def user_1():
     return auth_register_v1("mikey@unsw.com", "test", "Mikey", "Test")
+@fixture
 def user_2():
     return auth_register_v1("miguel@unsw.com", "test", "Miguel", "Test")
-
 @fixture
 def channel_1():
     return channels_create_v1(user_1["auth_user_id"], "Test Channel", True)
