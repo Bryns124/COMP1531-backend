da05c51f8270a8ccff6ef9dbbf120385264dbd54
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-04 00:33:58 +1100

    Wrote the channels_create function

--- src/channels.py
+++ src/channels.py
@@ -1,15 +1,17 @@
-import data_store
-import channel
+from src.data_store import data_store
+import src.channel
 
 def channels_list_v1(auth_user_id):
     store = data_store.get()
-    channel_list = {[]}
+    channel_list = []
     
-    for ids in store['users']:
-        if (auth_user_id == ids['user_id']):
-            channel_list.append(channel[ids])
+    for accounts in store['users']:
+        if auth_user_id == accounts['user_ids']:
+            channel_list = accounts['channels_owned']
+            channel_list += accounts['channels_joined']
             
     return channel_list
+
 
 
 def channels_listall_v1(auth_user_id):
@@ -23,18 +25,4 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
-    store = data_store.get()
-    store['channels']['name'].append(name)
-    store['channels']['is_public'].append(is_public)
-    
-    channel_id = len(store['channels'][channel_id]) - 1
-    store['channels']['channel_id'].append(channel_id)
-    store['channels']['channel_owner'].append(auth_user_id)
-    store['channels']['channel_members'].append([auth_user_id])
-    store['channels']['channel_messages'].append([])
-    
-    data_store.set(store)
-    
-    channel_join_v1(auth_user_id, channel_id)
-    
-    return channel_id
+    return 
