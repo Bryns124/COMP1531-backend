1bd934b4ae776ff5ec311c80acf8d98db9f1a432
Bryan Le <bryan.le@student.unsw.edu.au>
2022-03-04 00:04:19 +1100

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,12 +1,81 @@
+import re
 from src.data_store import data_store
 from src.error import InputError
+# yea not sure how to actually create the user for now
 
 def auth_login_v1(email, password):
+    if email_check(email) == False:
+        raise InputError
+    if duplicate_email_check(email) == False:
+        raise InputError
+    if password_check(password) == False:
+        raise InputError
+    
+    return {
+        'auth_user_id': 1,
+    }
+    
+# need to initialise a user to use the check functions.
+def auth_register_v1(email, password, name_first, name_last):
+    if email_check(email) == False:
+        raise InputError("Email entered is not a valid email")
+    if duplicate_email_check(email) == True:
+        raise InputError("Email entered has already been registered")
+    if len(password) < 6:
+        raise InputError("Password entered must be longer than 6 characters")
+    if len(name_first) < 1 and len(name_first) > 50:
+        raise InputError("First name entered must be between 1 and 50 characters inclusive")
+    if len(name_last) < 1 and len(name_last) > 50:
+        raise InputError("Last name entered must be between 1 and 50 characters inclusive")
+    
+    user = add_user(u_id, email, password, name_first, name_last)
+    
     return {
         'auth_user_id': 1,
     }
 
-def auth_register_v1(email, password, name_first, name_last):
-    return {
-        'auth_user_id': 1,
-    }
+# need to be able to actually create a user but not sure how for now
+def create_user(u_id, email, password, name_first, name_last):
+    user = {
+        'u_id' : u_id,
+        'email' : email, 
+        'password' : password,
+        'name_first' : name_first, 
+        'name_last' : name_last, 
+        'handle_str' : [],
+    } 
+    return user
+
+def add_user(u_id, email, password, name_first, name_last):
+    # not sure how i calculate u_id
+    u_id = 12
+    user = create_user(u_id, email, password, name_first, name_last)
+    data = data_store.get()
+    data['users'].append(user)
+    return user
+    
+###############################################################
+##                 Checking functions                        ##
+###############################################################
+def email_check(email):
+    regex = r'[A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,3}+'
+    if (re.search(regex, email)):
+        return True
+    else:
+        return False
+    
+def duplicate_email_check(email):
+    data = data_store.get()
+    for user in data['users']:
+        if user['email'] == email:
+            return True
+        else:
+            return False
+
+def password_check(password):
+    data = data_store.get()
+    for user in data['users']:
+        if user['password'] == password:
+            return user
+        else:
+            return False
