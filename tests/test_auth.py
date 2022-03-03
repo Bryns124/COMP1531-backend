86b8969284577b33b2ab00a059f480052c899427
z5361001 <z5361001@ad.unsw.edu.au>
2022-03-03 15:05:48 +1100

    Updated fixes to register tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,10 +4,6 @@ from src.auth import auth_register_v1, auth_login_v1
 from src.error import InputError
 from src.other import clear_v1
 
-# check that when you register it returns a unique user ID
-# check that when register, login doesnt return input error if correct.
-
-# REGISTERS
 # test when the registering email is invalid
 def test_register_invalid_email():
     with pytest.raises(InputError):
@@ -27,37 +23,37 @@ def test_register_email_already_used():
     with pytest.raises(InputError):
         assert auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "Le")
     clear_v1()
-    
+
 # test when a new email is registered that it returns a unique user ID
 def test_register_returns_unique_ID():
     assert auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "Le") != auth_register_v1("jamesnguyen@gmail.com", "password789", "James", "Nguyen")
     clear_v1()
 
-# test when the len(password) < 6 characters
+# test when the registered len(password) < 6 characters
 def test_password_length_less_than_6():
     with pytest.raises(InputError):
         assert auth_register_v1("bryanle@gmail.com", "pass", "Bryan", "Le")
     clear_v1()
 
-# test when the name_first < 1
+# test when the registered name_first < 1
 def test_first_name_length_less_than_1():
     with pytest.raises(InputError):
         assert auth_register_v1("bryanle@gmail.com", "password123", "", "Le")
     clear_v1()
         
-# test when the name_first > 50
+# test when the registered name_first > 50
 def test_first_name_length_more_than_50():
     with pytest.raises(InputError):
         assert auth_register_v1("bryanle@gmail.com", "password123", "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbn", "Le")
     clear_v1()
         
-# test when the name_last < 1
+# test when the registered name_last < 1
 def test_last_name_length_less_than_1():
     with pytest.raises(InputError):
         assert auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "")
     clear_v1()
 
-# test when the name_last > 50
+# test when the registered name_last > 50
 def test_last_name_length_more_than_50():
     with pytest.raises(InputError):
         assert auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbn")
