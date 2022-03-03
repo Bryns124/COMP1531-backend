1fd4483baacc3b75e2737edbcb79c412a37b22b3
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-04 01:37:33 +1100

    Wrote more tests for test_channels.py and changed the fixture functions and imports to match imports to match that of Alice's so that merge conflicts can be avoided

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -1,49 +1,59 @@
 import pytest
-
-from src.channels import channels_list_v1
-from src.channels import channels_create_v1
-from src.auth import auth_login_v1
+from src.channels import channels_listall_v1, channels_create_v1
 from src.auth import auth_register_v1
-from src.error import AccessError
-from src.error import InputError
+from src.channel import channel_join_v1
 from src.other import clear_v1
+from src.error import AccessError, InputError
+
 
 @pytest.fixture
-def register_user():
-    auth_register_v1("adi@gmail.com", "123456", "Adiyat", "Rahman")
+def user_1():
+    return auth_register_v1('alice@gmail.com', '123456', 'Alice', 'Wan') 
 
-@pytest.fixture 
+@pytest.fixture
+def user_2():
+    return auth_register_v1('adi@gmail.com', 'abcdef', 'Adiyat', 'Rahman') 
+
+@pytest.fixture #user1 creates a public channel
+def public_channel_user1(user_1):
+    return channels_create_v1(user_1['auth_user_id'], 'Public', True)
+
+@pytest.fixture #user2 creates a private channel
+def private_channel_user2(user_2):
+    return channels_create_v1(user_2['auth_user_id'], 'Private', False)
+
+@pytest.fixture #user2 joined a public channel
+def joined_channel(user_2, public_channel_user1):
+    return channel_join_v1(user_2['auth_user_id'], public_channel_user1['channel_id'])
+
+@pytest.fixture
 def clear_data():
     clear_v1()
-
-@pytest.fixture
-def public_channel():
-    channels_create_v1(register_user['auth_user_id'], 'Public', True)
-
-@pytest.fixture    
-def private_channel():
-    channels_create_v1(register_user['auth_user_id'], 'Private', False)
+    
     
 
 
-def test_create_public_channel(clear_data, user_id = register_user):
-    assert channels_create_v1(user_id, 'test_channel', True)['channel_id'] == 1
+def test_create_public_channel(clear_data, user_2):
+    '''
+    
+    '''
+    assert channels_create_v1(user_2['u_id'], 'public_channel', True)['channel_id'] == 1
     
 
-# def test_create_private_channel(clear_data, register_user):
-#     assert channels_create_v1(register_user['auth_user_id'], False)['channel_id'] == 1
-    
-# Alternative Way
-def test_create_private_channel(clear_data, user_id = register_user):
-    assert channels_create_v1(user_id, 'test_channel', False)['channel_id'] == 1
+def test_create_private_channel(clear_data, user_2):
+    assert channels_create_v1(user_2['u_id'], 'test_channel', False)['channel_id'] == 1
     
     
-def test_create_channel_invalid_name_1(clear_data, user_id = register_user):
+def test_create_channel_invalid_name_1(clear_data, user_2):
     with pytest.raises(InputError):
-        assert channels_create_v1(user_id, '', True)['channel_id'] == 1
+        assert channels_create_v1(user_2['u_id'], '', True)['channel_id']
         
         
-    
-def test_create_channel_invalid_name_2(clear_data, user_id = register_user):
+def test_create_channel_invalid_name_2(clear_data, user_2):
     with pytest.raises(InputError):
-        assert channels_create_v1(user_id, 'abcdefghijklmnopqrstuv', True)['channel_id'] == 1
+        assert channels_create_v1(user_2['u_id'], 'abcdefghijklmnopqrstuv', True)['channel_id']
+
+def test_create_multiple_channel(clear_data, user_2):
+        assert channels_create_v1(user_2['u_id'], 'channel_1', True)['channel_id'] == 1
+        assert channels_create_v1(user_2['u_id'], 'channel_2', True)['channel_id'] == 2
+        
