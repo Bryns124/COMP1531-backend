8bb24bea115527404b9e0256a98ffcc71d88f82d
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-05 11:18:29 +1100

    Fixed syntax errors

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,17 +1,19 @@
+import pytest
+
 from src.channel import channel_messages_v1, channel_invite_v1
 from src.channels import channels_create_v1
 from src.auth import auth_register_v1
 
-@fixture
+@pytest.fixture()
 def user_1():
     return auth_register_v1("mikey@unsw.com", "test", "Mikey", "Test")
-@fixture
+@pytest.fixture()
 def user_2():
     return auth_register_v1("miguel@unsw.com", "test", "Miguel", "Test")
-@fixture
+@pytest.fixture()
 def channel_1():
     return channels_create_v1(user_1["auth_user_id"], "Test Channel", True)
-@fixture 
+@pytest.fixture() 
 def starting_value():
     return 0
     
