61c4f6ef86e30ac8f12db248893f5e24d9898656
z5361001 <z5361001@ad.unsw.edu.au>
2022-03-05 11:57:22 +1100

    deleted register function from login branch

--- src/auth.py
+++ src/auth.py
@@ -24,45 +24,6 @@ def auth_login_v1(email, password):
             
     raise InputError("Email does not exist")
     
-def auth_register_v1(email, password, name_first, name_last):
-    if email_check(email) == False:
-        raise InputError("Email entered is not a valid email")
-    if duplicate_email_check(email) == True:
-        raise InputError("Email entered has already been registered")
-    if len(password) < 6:
-        raise InputError("Password entered must be longer than 6 characters")
-    if len(name_first) < 1 or len(name_first) > 50:
-        raise InputError("First name entered must be between 1 and 50 characters inclusive")
-    if len(name_last) < 1 or len(name_last) > 50:
-        raise InputError("Last name entered must be between 1 and 50 characters inclusive")
-
-    user = create_user(email, password, name_first, name_last)
-    return {
-        'auth_user_id': user['u_id']
-    }
-
-# need to be able to actually create a user but not sure how for now
-def create_user(email, password, name_first, name_last):
-    global store
-    new_id = len(store['users']) + 1
-    user = {
-        'u_id' : new_id, 
-        'email': email, 
-        'name_first': name_first, 
-        'name_last': name_last, 
-        'handle_str': "" , # generate later 
-        'password': password,
-        'channels_owned' : [], 
-        'channels_joined' : [],
-    }
-    store['users'].append(user)
-    data_store.set(store)
-    return user
-
-def create_handle(name_first, name_last):
-    
-    pass
-    
 ###############################################################
 ##                 Checking functions                        ##
 ###############################################################
