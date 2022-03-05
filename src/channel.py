f9bbf56881b408c0b1e883a9039fd765d5f8b4e1
Daniel <danielyung144@outlook.com>
2022-03-05 11:48:52 +1100

    Added comments

--- src/channel.py
+++ src/channel.py
@@ -12,28 +12,32 @@ store = data_store.get()
 def channel_details_v1(auth_user_id, channel_id):
     global store
 
-    channel_members = []
-    owner_members = []
     is_channel = False
     is_member = False
     
-    all_channels = store['channels']
+    all_channels = store['channels'] # Setting a list of all channels in data_store
 
+    # Checking that the inputted channel_id is from a channel that exists
     for channel in all_channel:
         if channel['channel_id'] == channel_id:
             is_channel = True
-            active_channel = channel
+            active_channel = channel # Saves the channel specified from channel_id
             break
     
+    # Checking that the inputted user is in the channel specified
     for member in active_channel['all_members']:
         if member['u_id'] == auth_user_id:
             is_member = True
             break
         
+    # Exceptions
+    # If the inputted channel_id is not from a channel saved in data_store, raises an InputError
     if not is_channel:
-        raise InputError() # fix this pls
+        raise InputError()
 
+    # If the inputted auth_user_id is not a member of the channel specified, raises an AccessError
     if not is_member:
         raise AccessError()
 
+    # return a tuple (name, is_public, owner_members, all_members)
     return (active_channel['channel_name'], active_channel['is_public'], active_channel['owner members'], active_channel['all_members'])
