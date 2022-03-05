62cd3501c7908e3f999a6ac9da521bf9636ddb4b
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-05 12:31:05 +1100

    removing test from branch which is not supposed to be there

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,19 +1,6 @@
 from src.channel import channel_messages_v1, channel_invite_v1
 from src.channels import channels_create_v1
 from src.auth import auth_register_v1
-
-@fixture
-def user_1():
-    return auth_register_v1("mikey@unsw.com", "test", "Mikey", "Test")
-@fixture
-def user_2():
-    return auth_register_v1("miguel@unsw.com", "test", "Miguel", "Test")
-@fixture
-def channel_1():
-    return channels_create_v1(user_1["auth_user_id"], "Test Channel", True)
-@fixture 
-def starting_value():
-    return 0
     
 def test_channel_messages(user_1, channel_1, starting_value):
     
