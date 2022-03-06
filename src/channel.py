62faba574ce1d37f0543b57137276a23ad2315dd
Daniel <danielyung144@outlook.com>
2022-03-06 12:10:51 +1100

    Some debugging

--- src/channel.py
+++ src/channel.py
@@ -4,40 +4,39 @@ from src.error import AccessError, InputError
 '''
 channel_details_v1(auth_user_id, channel_id) 
 
-returns (name, is_public, owner_members, all_members)
+returns {channel_name, is_public, owner_members, all_members}
 
 Given a channel with ID channel_id that the authorised user is a member of, provide basic details about the channel.
 '''
 store = data_store.get()
 def channel_details_v1(auth_user_id, channel_id):
-    global store
+    store = data_store.get()
 
     is_channel = False
     is_member = False
     
-    all_channels = store['channels'] # Setting a list of all channels in data_store
+    all_channels = store['channels']
 
-    # Checking that the inputted channel_id is from a channel that exists
-    for channel in all_channel:
+    for channel in all_channels:
         if channel['channel_id'] == channel_id:
             is_channel = True
-            active_channel = channel # Saves the channel specified from channel_id
+            active_channel = channel
             break
     
-    # Checking that the inputted user is in the channel specified
-    for member in active_channel['all_members']:
-        if member['u_id'] == auth_user_id:
-            is_member = True
-            break
-        
-    # Exceptions
-    # If the inputted channel_id is not from a channel saved in data_store, raises an InputError
     if not is_channel:
         raise InputError()
 
-    # If the inputted auth_user_id is not a member of the channel specified, raises an AccessError
+    for member in active_channel['all_members']:
+        if member == auth_user_id:
+            is_member = True
+            #break
+        
     if not is_member:
         raise AccessError()
 
-    # return a tuple (name, is_public, owner_members, all_members)
-    return (active_channel['channel_name'], active_channel['is_public'], active_channel['owner members'], active_channel['all_members'])
+    return {
+        'channel_name': active_channel['channel_name'],
+        'is_public': active_channel['is_public'],
+        'owner_members': active_channel['owner_members'],
+        'all_members': active_channel['all_members']
+    }
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,4 +1,4 @@
-from src.channel import channel_details_v1, channel_invite_v1, channel_join_v1
+from src.channel import channel_details_v1, channel_join_v1
 from src.channels import channels_create_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
@@ -7,19 +7,23 @@ import pytest
 
 @pytest.fixture
 def user_1():
-    return auth_register_v1("mikey@unsw.com", "test", "Mikey", "Test")
+    return auth_register_v1("mikey@unsw.com", "test123456", "Mikey", "Test")
 
 @pytest.fixture
 def user_2():
-    return auth_register_v1("miguel@unsw.com", "test", "Miguel", "Test")
+    return auth_register_v1("miguel@unsw.com", "test123456", "Miguel", "Test")
 
 @pytest.fixture
-def channel_1():
+def channel_1(user_1):
     return channels_create_v1(user_1["auth_user_id"], "A New Hope", True)
 
 @pytest.fixture
-def channel_2():
+def channel_2(user_2):
     return channels_create_v1(user_2["auth_user_id"], "The Empire Strikes Back", True)
+
+@pytest.fixture
+def invalid_user_id():
+    return -1
 
 @pytest.fixture
 def starting_value():
@@ -39,37 +43,49 @@ returns (name, is_public, owner_members, all_members)
 Given a channel with ID channel_id that the authorised user is a member of, provide basic details about the channel.
 '''
 
-def test_input_error_channel_details_v1():
+def test_input_error_channel_details_v1(user_1, invalid_channel):
     # returns InputError when invalid channel_id is provided
     with pytest.raises(InputError):
         channel_details_v1(user_1['auth_user_id'], invalid_channel['channel_id'])
+    clear_v1()
 
 
-def test_access_error_channel_details_v1():
+def test_access_error_channel_details_v1(user_2, channel_1):
     # returns AccessError when user is not a member of the channel
     with pytest.raises(AccessError):
         channel_details_v1(user_2['auth_user_id'], channel_1['channel_id'])
-
-def test_correct_inputs_channel_details_v1():
-    # Tests for correct inputs
-    assert channel_details_v1(user_1['auth_user_id'], channel_1['channel_id']) == (
-        "A New Hope", 
-        True, 
-        [{'u_id': 1, 'email': 'mikey@unsw.com' , 'name_first': 'Mikey', 'name_last': 'Test', 'handle_str': 'mikeytest'}], 
-        [{'u_id': 1, 'email': 'mikey@unsw.com' , 'name_first': 'Miikey', 'name_last': 'Test', 'handle_str': 'mikeytest'}]
-    )
     clear_v1()
 
-    channel_join_v1(user_1['auth_user_id'], channel_2['channel_id'])
-    assert channel_details_v1(user_1['auth_user_id'], channel_2['channel_id']) == (
-        "The Empire Strikes Back", 
-        True, 
-        [
-            {'u_id': 2, 'email': 'miguel@unsw.com' , 'name_first': 'Miguel', 'name_last': 'Test', 'handle_str': 'migueltest'}
-        ], 
-        [
-            {'u_id': 2, 'email': 'miguel@unsw.com' , 'name_first': 'Miguel', 'name_last': 'Test', 'handle_str': 'migueltest'},
+def test_wrong_user_id(invalid_user_id, channel_1):
+    with pytest.raises(InputError("Invalid User ID")):
+        channel_details_v1(invalid_user_id, channel_1)
+    clear_v1()
+
+def test_correct_inputs_channel_details_v1(user_1, channel_1):
+    # Tests for correct inputs
+    assert channel_details_v1(user_1['auth_user_id'], channel_1['channel_id']) == {
+        'channel_name': "A New Hope", 
+        'is_public': True, 
+        'owner_members': [
             {'u_id': 1, 'email': 'mikey@unsw.com' , 'name_first': 'Mikey', 'name_last': 'Test', 'handle_str': 'mikeytest'}
+            ], 
+        'all_members': [
+            {'u_id': 1, 'email': 'mikey@unsw.com' , 'name_first': 'Mikey', 'name_last': 'Test', 'handle_str': 'mikeytest'}
+            ]
+    }
+    clear_v1()
+
+def test_private_channel_details_v1(user_1, channel_2):
+    channel_join_v1(user_1['auth_user_id'], channel_2['channel_id'])
+    assert channel_details_v1(user_1['auth_user_id'], channel_2['channel_id']) == {
+        'channel_name': "The Empire Strikes Back", 
+        'is_public':  True, 
+        'owner_members': [
+            {'u_id': 1, 'email': 'miguel@unsw.com' , 'name_first': 'Miguel', 'name_last': 'Test', 'handle_str': 'migueltest'}
+        ], 
+        'all_members': [
+            {'u_id': 1, 'email': 'miguel@unsw.com' , 'name_first': 'Miguel', 'name_last': 'Test', 'handle_str': 'migueltest'},
+            {'u_id': 2, 'email': 'mikey@unsw.com' , 'name_first': 'Mikey', 'name_last': 'Test', 'handle_str': 'mikeytest'}
         ]
-    )
+    }
     clear_v1()
