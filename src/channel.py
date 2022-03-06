a70287c53ce43409f256cb895a3f9bfc2df4408d
Daniel <danielyung144@outlook.com>
2022-03-06 16:53:51 +1100

    Reset to local reso

--- src/channel.py
+++ src/channel.py
@@ -1,83 +1,14 @@
 from src.data_store import data_store
 from src.error import AccessError, InputError
+from src.channels import channels_list_v1
 
-def channel_invite_v1(auth_user_id, channel_id, u_id):
-    """
-    Allows a authorized user to invite another user to a channel they are apart of.
+'''
+channel_details_v1(auth_user_id, channel_id) 
 
-    Args:
-        auth_user_id (u_id): A valid user who is apart of the channel/
-        channel_id (channel_id): The channel id auth_user is inviting to.
-        u_id (u_id): A valid second user who is being invited
+Given a channel with ID channel_id that the authorised user is a member of, provide basic details about the channel.
 
-    Raises:
-        InputError: u_id dos not exist in datastore.
-        InputError: channel_id does not exist in datastore.
-        InputError: the invited user is already part of the channel.
-        AccessError: the auth_user is not in the channel they are inviting to.
-
-    Returns:
-        dictionary: nothing! nothing is returned after a invite 
-    """
-    store = data_store.get()
-    
-    auth_user_exist = False
-    user_exist = False
-    
-    for user in store['users']:
-        if auth_user_id == user['u_id']: 
-            auth_user_exist = True
-        elif u_id == user['u_id']:        
-            user_exist = True
-            
-    if auth_user_exist == False or user_exist == False:
-        raise InputError
-    
-    channel_exist = False
-    for channel in store['channels']:
-        if channel['channel_id'] == channel_id:
-            channel_exist = True
-            
-    if channel_exist == False:
-        raise InputError
-        
-    already_member = False
-    can_invite = False
-    for members in store['channels'][channel_id - 1]['all_members']:
-        if members == u_id:
-            already_member = True
-        elif members == auth_user_id:
-            can_invite = True
-    
-    if already_member == True:
-        raise InputError
-    
-    if can_invite == False:
-        raise AccessError
-            
-    for channel in store['channels']:
-        for user in store['users']:
-            if u_id == user['u_id']:
-                invited_member = user['u_id']
-                user['channels_joined'].append(channel)
-        if channel["channel_id"] == channel_id: 
-            channel['all_members'].append(invited_member)
-        
-            
-    # print(store['channels'])
-        
-    data_store.set(store)
-    return {
-    }
-
-def channel_details_v1(auth_user_id, channel_id):
-    '''
-    channel_details_v1(auth_user_id, channel_id) 
-
-    Given a channel with ID channel_id that the authorised user is a member of, provide basic details about the channel.
-
-    returns a dictionary 
-    {
+returns a dictionary 
+{
     "channel_name": name of the channel (string),
     "is_public": whether or not the channel is public (boolean),
     "owner_members": a list of dictionaries containing owner users, each dictionary being of the form: {
@@ -91,6 +22,9 @@ def channel_details_v1(auth_user_id, channel_id):
     on all members of the channel
 }
 '''
+
+def channel_details_v1(auth_user_id, channel_id):
+    
     store = data_store.get() # Accessing data_store for data
 
     is_channel = False # Initialising booleans for raising errors
@@ -184,6 +118,7 @@ def member_details(user_id):
     # returns None if user is not found, 
     return
 
+
 def channel_join_v1(auth_user_id, channel_id):
     store = data_store.get()
     store['channels'][channel_id - 1]['all_members'].append(auth_user_id)
@@ -234,61 +169,10 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
                 user['channels_joined'].append(channel)
         if channel["channel_id"] == channel_id: 
             channel['all_members'].append(invited_member)
-         
+        
+            
     # print(store['channels'])
         
     data_store.set(store)
     return {
     }
-    '''
-    function allows user to join another channel based on the channel ID
-    will give input error if the channel id is invalid or if the user is already in the channel
-    will give access error if the channel ID is one for a private channel
-    '''
-    store = data_store.get()
-        
-    if channel_validity(channel_id, store) == False:
-        raise InputError("Channel id is invalid.")
-    
-    if already_member(auth_user_id, channel_id, store) == True:
-        raise InputError("The user is already a member of this channel.")
-    
-    current_channel = extract_channel_details(channel_id, store)
-    if current_channel['is_public'] == False:
-        raise AccessError("This is a private channel, user does not have access.")
-
-
-    for user_accounts in store['users']:
-        if user_accounts['u_id'] == auth_user_id:
-            new_member = user_accounts
-            user_accounts['channels_joined'].append(current_channel)
-    
-    for channels in store['channels']:
-        if channels['channel_id'] == channel_id:
-            channels['all_members'].append(new_member)
-            
-    data_store.set(store)
-    return
-
-
-def channel_validity(channel_id, store):
-    for channels in store['channels']:
-        if channels['channel_id'] == channel_id:
-            return True
-    return False
-
-
-def already_member(auth_user_id, channel_id, store):
-    for channels in store['channels']:
-        if channels['channel_id'] == channel_id:
-            if auth_user_id in channels['all_members'] or auth_user_id in channels['owner_members']:
-                return True    
-    return False
-
-
-def extract_channel_details(channel_id, store):
-    for channels in store['channels']:
-        if channels['channel_id'] == channel_id:
-            channel_details = channels
-    return channel_details    
-
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -1,46 +1,27 @@
-from enum import auto
-from src.channel import channel_messages_v1, channel_invite_v1, channel_details_v1, channel_join_v1
-from src.channels import channels_create_v1, channels_list_v1
-from src.auth import auth_register_v1, auth_login_v1
-from src.error import AccessError, InputError 
+from src.channel import channel_details_v1, channel_join_v1
+from src.channels import channels_create_v1
+from src.auth import auth_register_v1
 from src.other import clear_v1
+from src.error import InputError, AccessError
 import pytest
 
 
-"""Users"""
-@pytest.fixture()
+# Adding fixtures for users and channels, including invalids to use for tests 
+@pytest.fixture
 def user_1():
     return auth_register_v1("mikey@unsw.com", "test123456", "Mikey", "Test")
-@pytest.fixture 
+
+@pytest.fixture
 def user_2():
     return auth_register_v1("miguel@unsw.com", "test123456", "Miguel", "Test")
-@pytest.fixture
-def user_no_access():
-    return auth_register_v1("error@unsw.com", "no_access1235667", "no_access", "no_access")
-@pytest.fixture
-def user_invalid():
-    return "invalid"
 
-"""Channels"""
-@pytest.fixture
-def channel_public(user_1):
-    return channels_create_v1(user_1["auth_user_id"], "Test Channel", True)
-@pytest.fixture
-def channel_private_access(user_no_access):
-    return channels_create_v1(user_no_access["auth_user_id"], "No Access Channel", False)
 @pytest.fixture
 def channel_1(user_1):
     return channels_create_v1(user_1["auth_user_id"], "A New Hope", True)
+
 @pytest.fixture
 def channel_2(user_2):
     return channels_create_v1(user_2["auth_user_id"], "Empire Strikes Back", True)
-def channel_private(user_1):
-    return channels_create_v1(user_1["auth_user_id"], "Private Channel", False)
-@pytest.fixture
-def invalid_channel_id():
-    return -1
-
-    
 
 @pytest.fixture
 def invalid_user_id():
@@ -55,73 +36,6 @@ def invalid_channel():
     return {
         'channel_id': -1
     } 
-   
-def test_channel_invite_access_error(user_1, channel_private_access, user_2):    
-    """
-    This test checks to see that a AccessError is raised when attmepting to invite someone to a channel,
-    but they do not have the access privlieges to do so. 
-    Args:
-        user_1 (u_id): User who is not apart of the private channel
-        channel_private (channel_id): The private channel_id
-        user_no_access (u_id): User who is going to  be invited 
-    """
-    with pytest.raises(AccessError):
-        channel_invite_v1(user_1["auth_user_id"],channel_private_access['channel_id'], user_2["auth_user_id"])
-    clear_v1()
-
-def test_channel_invite_channel_id_error(user_1, invalid_channel_id, user_2):
-    """
-    This test checks to see that a InputError is raised when attemtping to invite someone to
-    a channel with a invalid channel_id
-    Args:
-        user_1 (_type_): The u_id of the person executing the command and inviting. 
-        invalid_channel_id (_type_): The invalid channel_id
-        user_2 (_type_): The u_id of the person being invited
-    """
-    with pytest.raises(InputError):
-        channel_invite_v1(user_1["auth_user_id"], invalid_channel_id, user_2["auth_user_id"])
-    clear_v1()
-
-def test_channel_invite_u_id_error(user_1, channel_public, user_invalid):
-    """
-    This test checks to see that a InputError is raised when attemtping to invite someone with
-    an invalid u_id.
-    Args:
-         user_1 (u_id): The u_id of the person executing the command and inviting. 
-        channel_public (channel_id): Takes the channel_id that user_1 is inviting to.  
-        user_invalid (u_id): The invalid u_id
-    """
-    with pytest.raises(InputError):
-        channel_invite_v1(user_1['auth_user_id'], channel_public['channel_id'], user_invalid) 
-        channel_invite_v1(user_invalid, channel_public['channel_id'], user_1['auth_user_id'])
-    clear_v1()
-        
-def test_channel_invite_u_id_member(user_1, channel_public, user_2):
-    """
-    This test checks to see that a InputError is raised when attempting invite someone 
-    to a channel who is already apart of that channel.
-    Args:
-        user_1 (u_id): The u_id of the person executing the command and inviting. 
-        channel_public (channel_id): Takes the channel_id that user_1 is inviting to.  
-        user_2 (u_id): The u_id of the person that is already in the channel.
-    """
-    channel_invite_v1(user_1['auth_user_id'], channel_public['channel_id'], user_2['auth_user_id'])
-    with pytest.raises(InputError):
-        channel_invite_v1(user_1['auth_user_id'], channel_public['channel_id'], user_2['auth_user_id'])
-    clear_v1()
-    
-
-def test_channel_invite(user_1, channel_public, user_2):
-    """
-    This test verifies that when a user is invited to a channel they successfully become a member
-    Args:
-        user_1 (u_id): The u_id of the person executing the command and inviting. 
-        channel_public (channel_id): Takes the channel_id that user_1 is inviting to.  
-        user_2 (u_id): The u_id of the person being invited.
-    """
-    channel_invite_v1(user_1['auth_user_id'], channel_public['channel_id'], user_2['auth_user_id'])
-    assert channels_list_v1(user_2['auth_user_id'])['channels'][-1]['channel_id'] == channel_public['channel_id']
-    clear_v1()
 
 '''
 channel_details_v1(auth_user_id, channel_id) 
@@ -183,6 +97,7 @@ def test_multiple_user_channel_details_v1(user_1, channel_2):
     
     # Adds another user to the channel, who doesn't have owner status
     channel_join_v1(user_1['auth_user_id'], channel_2['channel_id']) 
+
     assert channel_details_v1(user_1['auth_user_id'], channel_2['channel_id']) == {
         'channel_name': "Empire Strikes Back", 
         'is_public':  True, 
@@ -194,45 +109,4 @@ def test_multiple_user_channel_details_v1(user_1, channel_2):
             {'u_id': 1, 'email': 'mikey@unsw.com' , 'name_first': 'Mikey', 'name_last': 'Test', 'handle_str': 'mikeytest'}
         ]
     }
-    clear_v1()
-
-def test_channel_join_invalid_channel(user_1):
-    '''
-    channel_id does not refer to a valid channel
-    '''
-    with pytest.raises(InputError):
-        channel_join_v1(user_1['auth_user_id'], -1)
-    clear_v1()
-
-def test_channel_join_already_member(user_1, channel_public, channel_private):
-    '''
-    the authorised user is already a member of the channel
-    '''
-    with pytest.raises(InputError):
-        channel_join_v1(user_1['auth_user_id'], channel_public['channel_id'])
-        channel_join_v1(user_1['auth_user_id'], channel_private['channel_id'])
-    clear_v1()
-
-def test_channel_join_access_private(user_1, user_2, channel_private):
-    '''
-    channel_id refers to a channel that is private 
-    and the authorised user is not already a channel member 
-    and is not a global owner
-    '''
-    with pytest.raises(AccessError):
-        channel_join_v1(user_2['auth_user_id'], channel_private['channel_id'])
-    clear_v1()
-
-def test_channel_join_success(user_1, user_2, channel_public):
-    '''
-    user successfully joins a public channel
-
-    Assumption: the joined channel will be added sequentially as the last added one
-    to channels list
-    '''
-    channel_join_v1(user_2['auth_user_id'], channel_public['channel_id'])
-    user_2_channels = channels_list_v1(user_2['auth_user_id'])
-    joined_channel = user_2_channels['channels'][-1]
-    assert channel_public['channel_id'] == joined_channel['channel_id']
-
     clear_v1()
