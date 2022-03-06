fc388b671f4c0debb6862a8ce1941b7fa2fd208a
z5361001 <z5361001@ad.unsw.edu.au>
2022-03-06 13:32:25 +1100

    edited comments for test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,7 +4,7 @@ from src.auth import auth_register_v1, auth_login_v1
 from src.error import InputError
 from src.other import clear_v1
 
-# test when the registering email is invalid
+# Testing when the registering email is invalid
 def test_register_invalid_email():
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmailcom", "password123", "Bryan", "Le")
@@ -17,44 +17,43 @@ def test_register_invalid_email():
         auth_register_v1("", "password123", "Bryan", "Le")
     clear_v1()
 
-# test when the registering email has already been used by another user
+# Testing when the registering email has already been used by another user
 def test_register_email_already_used():
     auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "Le")
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "Le")
     clear_v1()
 
-# test when a new email is registered that it returns a unique user ID
+# Testing that a new registered email returns a unqiue user ID
 def test_register_returns_unique_ID():
     assert auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "Le") != auth_register_v1("jamesnguyen@gmail.com", "password789", "James", "Nguyen")
     clear_v1()
 
-# test when the registered len(password) < 6 characters
+# Testing when the registered length of the password is less than 6 characters
 def test_password_length_less_than_6():
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmail.com", "pass", "Bryan", "Le")
     clear_v1()
 
-# test when the registered name_first < 1
+# Testing when the registered first name is less than 1
 def test_first_name_length_less_than_1():
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmail.com", "password123", "", "Le")
     clear_v1()
         
-# test when the registered name_first > 50
+# Testing when the registered first name is more than 50
 def test_first_name_length_more_than_50():
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmail.com", "password123", "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbn", "Le")
     clear_v1()
         
-# test when the registered name_last < 1
+# Testing when the registered last name is less than 1
 def test_last_name_length_less_than_1():
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "")
     clear_v1()
 
-# test when the registered name_last > 50
+# Testing when the registered last name is more than 50
 def test_last_name_length_more_than_50():
     with pytest.raises(InputError):
         auth_register_v1("bryanle@gmail.com", "password123", "Bryan", "qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbn")
-    clear_v1()
