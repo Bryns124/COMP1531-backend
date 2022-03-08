5b96e68f1c10660c14d4e95a8c734683f493b2ea
z5361001 <z5361001@ad.unsw.edu.au>
2022-03-08 19:25:21 +1100

    fixed handle alphanumeric error

--- src/auth.py
+++ src/auth.py
@@ -2,41 +2,22 @@ import re
 from src.data_store import data_store
 from src.error import InputError
 
+store = data_store.get()
+
 def auth_login_v1(email, password):
-    '''
-    Logs in a registered user given an email and password.
-    
-    :param email: the user's email
-    :param password: the user's password
-    :return: the user's user ID
-    :rtype: dictionary
-    '''
-    store = data_store.get()
-    # Check if user exists.
+    global store
     for user in store['users']:
         if user['email'] == email:
-            # Check if user's password entered is correct.
             if user['password'] == password:
                 return {
                     'auth_user_id': user['u_id']
                 }
             else:
-                raise InputError("Password entered is incorrect")
-
-    raise InputError("Email entered does not exist")
+                raise InputError("Password is incorrect")
+            
+    raise InputError("Email does not exist")
 
 def auth_register_v1(email, password, name_first, name_last):
-    '''
-    Registers in a new user given the email, password, first name and last name.
-    
-    :param email: the user's email
-    :param password: the user's password
-    :name_first: the user's first name
-    :name_last: the user's last name
-    :return: the user's user ID
-    :rtype: dictionary
-    '''
-    # Check if user's details are valid.
     if email_check(email) == False:
         raise InputError("Email entered is not a valid email")
     if duplicate_email_check(email) == True:
@@ -53,20 +34,10 @@ def auth_register_v1(email, password, name_first, name_last):
         'auth_user_id': user['u_id']
     }
 
+# need to be able to actually create a user but not sure how for now
 def create_user(email, password, name_first, name_last):
-    '''
-    Initialises the user's details and saves it into data_store. 
-    
-    :param email: the user's email
-    :param password: the user's password
-    :name_first: the user's first name
-    :name_last: the user's last name
-    :return: the user's details
-    :rtype: dictionary
-    '''
-    store = data_store.get()
+    global store
     new_id = len(store['users']) + 1
-    
     user = {
         'u_id' : new_id, 
         'email': email, 
@@ -77,29 +48,17 @@ def create_user(email, password, name_first, name_last):
         'channels_owned' : [], 
         'channels_joined' : [],
     }
-    
     store['users'].append(user)
     data_store.set(store)
     return user
 
 def create_handle(name_first, name_last):
-    '''
-    Creates the user's handle with the first name and last name. 
-    If the user's handle is more than 20 characters, it is cut off at 20 characters
-    If the user's handle already exists, append the handle with the smallest number (starting from 0).
-    
-    :name_first: the user's first name
-    :name_last: the user's last name
-    :return: the user's handle
-    :rtype: string
-    '''
-    store = data_store.get()
+    global store
     
     handle = name_first.lower() + name_last.lower()
+    handle = ''.join(filter(str.isalnum, handle))
     handle = handle[:20]
     
-    # Check if the user's handle already exists.
-    # Append the smallest number to the handle if handle already exists.
     i = 0
     for user in store['users']:
         if user['handle_str'] == handle:
@@ -111,15 +70,7 @@ def create_handle(name_first, name_last):
 ###############################################################
 ##                 Checking functions                        ##
 ###############################################################
-
 def email_check(email):
-    '''
-    Checks if the email entered is valid.
-    
-    :email: the user's email
-    :return: whether the email is valid or not
-    :rtype: boolean
-    '''
     regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
     if (re.fullmatch(regex, email)):
         return True
@@ -127,17 +78,17 @@ def email_check(email):
         return False
     
 def duplicate_email_check(email):
-    '''
-    Checks if the email entered has already been registered.
-    
-    :email: the user's email
-    :return: whether the email is new or already has been registered
-    :rtype: boolean
-    '''
-    store = data_store.get()
-    # Check if user's email already exists
+    global store
     for user in store['users']:
         if user['email'] == email:
             return True
+        else:
+            return False
+
+def password_check(password):
+    global store
+    for user in store['users']:
+        if user['password'] == password:
+            return user
         else:
             return False
