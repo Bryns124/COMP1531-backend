5fb8564b36ecfbe243096975efa15b84b423c820
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-09 18:44:33 +1100

    Fixing pylint errors

--- src/channels.py
+++ src/channels.py
@@ -9,23 +9,23 @@ def channels_list_v1(auth_user_id):
     store = data_store.get()
 
     auth_user_exist = False
-    
+
     for user in store['users']:
-        if auth_user_id == user['u_id']: 
+        if auth_user_id == user['u_id']:
             auth_user_exist = True
-    
+
     if auth_user_exist == False:
         raise AccessError
 
     for accounts in store['users']:
         if accounts['u_id'] == auth_user_id:
-            output_list = create_list_dictionary(accounts)        
+            output_list = create_list_dictionary(accounts)
     return {
         'channels': output_list
     }
 
 ''' Helper function:
-    Appends the list of dictionaries stored in the 'channels_owned' 
+    Appends the list of dictionaries stored in the 'channels_owned'
     and 'channels_joined' dictionaries in the data store, representing all
     the channels the user is associated with.
 '''
@@ -37,7 +37,7 @@ def create_list_dictionary(accounts):
                 'channel_name': owned['channel_name']
             }
             output_list.append(channel)
-        
+
     for joined in accounts['channels_joined']:
         channel = {
             'channel_id': joined['channel_id'],
@@ -58,11 +58,11 @@ def channels_listall_v1(auth_user_id):
     store_channels = store['channels']
 
     auth_user_exist = False
-    
+
     for user in store['users']:
         if auth_user_id == user['u_id']:
             auth_user_exist = True
-    
+
     if auth_user_exist == False:
         raise AccessError
 
@@ -73,7 +73,7 @@ def channels_listall_v1(auth_user_id):
         		'name': channel['channel_name'],
         	}
         all_channels.append(channel_dict)
-    
+
     return {
         'channels': all_channels
     }
@@ -84,47 +84,47 @@ def channels_listall_v1(auth_user_id):
     of this function is the id of that channel
 '''
 def channels_create_v1(auth_user_id, name, is_public):
-    
+
     store = data_store.get()
-    
+
     auth_user_exist = False
-    
+
     for user in store['users']:
-        if auth_user_id == user['u_id']: 
+        if auth_user_id == user['u_id']:
             auth_user_exist = True
-    
+
     if auth_user_exist == False:
         raise AccessError
-    
+
     if len(name) > 20:
         raise InputError("The name of the channel cannot be more than 20 characters.")
-    
+
     if len(name) < 1:
         raise InputError("The name of the channel cannot be less than 1 character.")
-    
+
     if store == {}:
         new_channel_id = 1
     else:
         new_channel_id = len(store['channels']) + 1
-    
+
     new_channel = {
-        'channel_id' : new_channel_id, 
+        'channel_id' : new_channel_id,
         'channel_name' : name,
         'is_public' : is_public,
-        'owner_members' : [auth_user_id], 
+        'owner_members' : [auth_user_id],
         'all_members' : [auth_user_id],
         'messages' : [],
-        'start' : 0, #ditto 
+        'start' : 0, #ditto
         'end' : 50,
     }
-    
+
     for users in store['users']:
         if users['u_id'] == auth_user_id:
             users['channels_owned'].append(new_channel)
-            
+
     store['channels'].append(new_channel)
     data_store.set(store)
-    
+
     return {
         'channel_id' : store['channels'][-1]['channel_id']
     }
