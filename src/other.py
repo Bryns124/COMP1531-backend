7acd217df8c06f04581ba537e0eab909c13d2814
Alice <z5362790@ad.unsw.edu.au>
2022-03-09 17:54:19 +1100

    added docstrings and passes pylint

--- src/other.py
+++ src/other.py
@@ -1,7 +1,17 @@
+"""Contains clear function to clear datastore
+
+clear: clears datastore
+"""
 from src.data_store import data_store
 
-#resets all data from date_store to initial stage
 def clear_v1():
+    """resets all data from data_store to initial stage
+
+    Args: None
+
+    Returns:
+        empty dictionary: {} as this is the inital stage of data_store
+    """
     store = data_store.get()
     store['users'] = []
     store['channels'] = []
