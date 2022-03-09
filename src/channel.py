e06dccc1a8ae06d74716b73896c8f7b8b33588fe
Michael Chai <michael.chai@student.unsw.edu.au>
2022-03-10 00:26:57 +1100

    Update channel.py

--- src/channel.py
+++ src/channel.py
@@ -165,7 +165,7 @@ def channel_details_v1(auth_user_id, channel_id):
     # returns a dictionary with the format specified in the docstring for this
     # function
     return {
-        'channel_name': active_channel['channel_name'],
+        'name': active_channel['name'],
         'is_public': active_channel['is_public'],
         'owner_members': owner_members_details,
         'all_members': all_members_details
