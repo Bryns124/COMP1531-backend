5aabf0d54a55b09bdb0ce3b2a25de1a4444c505c
Alice <z5362790@ad.unsw.edu.au>
2022-03-15 13:30:39 +1100

    added register route - practice ver.

--- src/server.py
+++ src/server.py
@@ -5,6 +5,8 @@ from flask import Flask, request
 from flask_cors import CORS
 from src.error import InputError
 from src import config
+from src.auth import auth_login_v1, auth_register_v1
+
 
 def quit_gracefully(*args):
     '''For coverage'''
@@ -30,6 +32,14 @@ APP.register_error_handler(Exception, defaultHandler)
 #### NO NEED TO MODIFY ABOVE THIS POINT, EXCEPT IMPORTS
 
 # Example
+@APP.route("/auth/register/v2", methods=['POST'])
+def auth_register_v2():
+    data = request.get_json()
+    ret = auth_register_v1(data['email'], data['password'], data['name_first'], data['hname_last'])
+    return dumps({
+        'auth_user_id': ret[auth_user_id]
+    })
+
 @APP.route("/echo", methods=['GET'])
 def echo():
     data = request.args.get('data')
