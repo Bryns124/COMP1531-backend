9572dd87eb3fea85db48503b9fa7571fe11055ba
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-18 21:54:42 +1100

    made some changes to how data is stored - eddited comment for owner and all member and changed how  messages are stored

--- tests/test_message.py
+++ tests/test_message.py
@@ -1,11 +1,19 @@
+from distutils.command.config import config
 from src.channel import channel_details_v1, channel_join_v1, channel_invite_v1, channel_messages_v1
 from src.channels import channels_create_v1, channels_list_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 from src.error import InputError, AccessError
 from src.helper import SECRET
+from src.config import port
+import json
+import requests
+import urllib
 import jwt
 import pytest
+
+##MAY CHANGE PORT LATER##
+BASE_URL = f"http://127.0.0.1:{port}/"
 
 
 """Users"""
@@ -57,3 +65,22 @@ def invalid_channel_id():
 @pytest.fixture
 def starting_value():
     return 0
+
+
+def test_channel_messages(user_1, channel_public, starting_value):
+    r = requests.get(f"{BASE_URL}/channel/messages/v2", json={
+        user_1['token'],
+        channel_public,
+        starting_value
+    })
+    payload = r.json()
+    assert payload 
+
+def test_login():
+    r = requests.post(f"{BASE_URL}/auth/login/v2", json={
+        "email": "alice@gmail.com",
+        "password": "123456"
+    })
+    payload = r.json()
+    assert payload["token"] == "token"  # sample token for now
+    assert payload["token"] == 1
