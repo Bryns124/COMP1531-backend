20b6cbe740b1eb6586aa0279db49801de913490a
Alice <z5362790@ad.unsw.edu.au>
2022-03-18 20:46:48 +1100

    added stub code

---
+++ src/message.py
@@ -0,0 +1,12 @@
+from src.data_store import data_store
+
+def message_send_v1(token, channel_id, message):
+    pass
+#{message_id}
+
+def message_edit_v1(token, message_id, message):
+    pass
+#{}
+def message_remove_v1(token, message_id):
+    pass
+#{}
