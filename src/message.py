02b8f6c1cc0e2056ccf9d5e672eedff383aca26e
Alice <z5362790@ad.unsw.edu.au>
2022-03-19 09:18:41 +1100

    wrote edit function and a bit of remove but may update if data_store is updated

--- src/message.py
+++ src/message.py
@@ -5,8 +5,27 @@ def message_send_v1(token, channel_id, message):
 #{message_id}
 
 def message_edit_v1(token, message_id, message):
-    pass
+    store = data_store.get()
+
+    for message in store['messages']:
+        if message['message_id'] == message_id:
+            message['message'] == message
+
+    data_store.set(store)
+
+    return {}
 #{}
 def message_remove_v1(token, message_id):
-    pass
+    store = data_store.get()
+
+    for message in store['messages']:
+        if message['message_id'] == message_id:
+            if (message['is_cnanel']):
+
+
+    data_store.set(store)
+    return {}
 #{}
+
+def do message_remove(message_id):
+    for channel in store['channels']:
