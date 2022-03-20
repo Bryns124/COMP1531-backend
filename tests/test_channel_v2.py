c86dd613d474fa5ad0474368b246636a0d735380
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-20 23:54:38 +1100

    fixef variable error

--- tests/test_channel_v2.py
+++ tests/test_channel_v2.py
@@ -191,7 +191,7 @@ def test_channel_join(channel_public, user_2):
         "channel_id": channel_public
     })
     r = requests.get(f"{BASE_URL}/channel/details/v2", json={
-        "token": user_1['token'],
+        "token": user_2['token'],
         "channel_id": channel_public
     })
     data = r.json()
