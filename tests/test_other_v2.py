d3fce0faa4a03e9744f6c3d270bffa6e744b3f5e
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-20 15:34:59 +1100

    formatting white space

--- tests/test_other_v2.py
+++ tests/test_other_v2.py
@@ -20,40 +20,44 @@ BASE_URL = f"http://127.0.0.1:{port}/"
 
 @pytest.fixture()
 def user_1():
-    return requests.post(f"{BASE_URL}/auth/register/v2", json={
+    r = requests.post(f"{BASE_URL}/auth/register/v2", json={
         "email": "mikey@unsw.com",
         "password": "test123456",
         "name_first": "Mikey",
         "name_last": "Test"
     })
+    return r.json()
 
 
 @pytest.fixture
 def user_2():
-    return requests.post(f"{BASE_URL}/auth/register/v2", json={
+    r = requests.post(f"{BASE_URL}/auth/register/v2", json={
         "email": "miguel@unsw.com",
         "password": "test123456",
         "name_first": "Miguel",
         "name_last": "Test"
     })
+    return r.json()
 
 
 @pytest.fixture
 def channel_public(user_1):
-    return requests.post(f"{BASE_URL}/channels/create/v2", json={
+    r = requests.post(f"{BASE_URL}/channels/create/v2", json={
         "token": user_1['token'],
         "name": "Test Channel",
         "is_public": True
     })
+    return r.json()
 
 
 @pytest.fixture
 def message_send(user_1, channel_public):
-    return requests.post(f"{BASE_URL}/messages/send/v1", json={
+    r = requests.post(f"{BASE_URL}/messages/send/v1", json={
         "token": user_1['token'],
         "channel_id": channel_public,
         "message": "Hello world"
     })
+    return r.json()
 
 
 @pytest.fixture
@@ -65,55 +69,55 @@ def DM(user_1):
 
 
 def test_clear_empty():
-    r = requests.delete(f"/clear/v1", json={
+    r = requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
     assert r.json() == {}
     assert r.status_code == 200
-    requests.delete(f"/clear/v1", json={
+    requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
 
 
 def test_clear_user(user_1, user_2):
-    r = requests.delete(f"/clear/v1", json={
+    r = requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
     assert r.json() == {}
     assert r.status_code == 200
-    requests.delete(f"/clear/v1", json={
+    requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
 
 
 def test_clear_channel(user_1, user_2, channel_public, channel_private):
-    r = requests.delete(f"/clear/v1", json={
+    r = requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
     assert r.json() == {}
     assert r.status_code == 200
-    requests.delete(f"/clear/v1", json={
+    requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
 
 
 def test_clear_messages(channel_public, message_send):
-    r = requests.delete(f"/clear/v1", json={
+    r = requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
     assert r.json() == {}
     assert r.status_code == 200
-    requests.delete(f"/clear/v1", json={
+    requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
 
 
 def test_clear_DM(DM):
-    r = requests.delete(f"/clear/v1", json={
+    r = requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
     assert r.json() == {}
     assert r.status_code == 200
-    requests.delete(f"/clear/v1", json={
+    requests.delete(f"{BASE_URL}/clear/v1", json={
 
     })
