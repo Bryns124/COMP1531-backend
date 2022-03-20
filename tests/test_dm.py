7c79dfeae049b053bfc2e0146ccba6b81738c615
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-21 09:38:15 +1100

    Wrote tests for dm_list

--- tests/test_dm.py
+++ tests/test_dm.py
@@ -36,7 +36,7 @@ def user_2():
     return r.json
 
 @pytest.fixture
-def user_2():
+def user_3():
     r = request.post(f"{BASE_URL}/auth/register/v2", json = {
         "email": "michael@gmail.com",
         "password": "1234567788",
@@ -45,7 +45,22 @@ def user_2():
     })
     return r.json
 
+@pytest.fixture
+def create_dm_2_user(user1, user2):
+    r = request.post(f"{BASE_URL}/dm/create/v1", json = {
+        "token": user1['token'],
+        "u_ids[]": [int(user2["auth_user_id"])]
+    })
+    return r.json()
 
+
+@pytest.fixture
+def create_dm_3_user(user1, user2, user3):
+    r = request.post(f"{BASE_URL}/dm/create/v1", json = {
+        "token": user1['token'],
+        "u_ids[]": [int(user2["auth_user_id"]), int(user3["auth_user_id"])]
+    })
+    return r.json()
 
 def test_dm_create_2_users(user1, user2):
     r = request.post(f"{BASE_URL}/dm/create/v1", json={
@@ -106,8 +121,137 @@ def test_dm_create_duplicate_ids_1(user1):
 
 def test_dm_create_duplicate_ids_2(user1, user2):
     r = request.post(f"{BASE_URL}/dm/create/v1", json={
-        "token": user1('token'),
+        "token": user1("token"),
         "u_ids[]": [int(user2('auth_user_id')), int(user2('auth_user_id'))]
     })
     assert r.status_code == InputError.code
+    clear_v1()
+
+def test_dm_list_empty(user1):
+    r = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user1("token")
+    })
+
+    payload = r.json()
+    assert payload == []
+    clear_v1()
+
+
+def test_dm_list_two_users(user1, user2, create_dm_2_users):
+    response_1 = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user1("token")
+    })
+
+    response_2 = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user2("token")
+    })
+
+    payload_1 = response_1.json()
+    payload_2 = response_2.json()
+    assert payload_1 == [{
+            'DM_id' : 1,
+            'DM_name' : "adiyatrahman, alicewan",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user2["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    }]
+
+    assert payload_2 == [{
+            'DM_id' : 1,
+            'DM_name' : "adiyatrahman, alicewan",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user2["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    }]
+    clear_v1()
+
+
+def test_dm_list_three_users(user1, user2, user3, create_dm_3_users):
+    response_1 = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user1("token")
+    })
+
+    response_2 = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user2("token")
+    })
+
+    response_3 = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user3("token")
+    })
+
+    payload_1 = response_1.json()
+    payload_2 = response_2.json()
+    payload_3 = response_3.json()
+
+    assert payload_1 == [{
+            'DM_id' : 1,
+            'DM_name' : "adiyatrahman, alicewan, michaelchai",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user2["auth_user_id"], user3["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    }]
+
+    assert payload_2 == [{
+            'DM_id' : 1,
+            'DM_name' : "adiyatrahman, alicewan, michaelchai",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user2["auth_user_id"], user3["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    }]
+
+    assert payload_3 == [{
+            'DM_id' : 1,
+            'DM_name' : "adiyatrahman, alicewan, michaelchai",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user2["auth_user_id"], user3["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    }]
+    clear_v1()
+
+
+def test_dm_list_two_dms(user1, user2, user3):
+    response_1 = request.post(f"{BASE_URL}/dm/create/v1", json = {
+        "token": user1['token'],
+        "u_ids[]": [int(user2["auth_user_id"])]
+    })
+
+    response_2 = request.post(f"{BASE_URL}/dm/create/v1", json = {
+        "token": user1['token'],
+        "u_ids[]": [int(user3["auth_user_id"])]
+    })
+
+    response_3 = request.get(f"{BASE_URL}/dm/list/v1", json = {
+        "token": user1("token")
+    })
+
+    payload = response_3.json()
+
+    assert payload == [{
+            'DM_id' : 1,
+            'DM_name' : "adiyatrahman, alicewan",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user2["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    },
+    {
+            'DM_id' : 2,
+            'DM_name' : "adiyatrahman, michaelchai",
+            'owner_members' : [user1["auth_user_id"]], #check again if this is leagal
+            'all_members' : [user1["auth_user_id"], user3["auth_user_id"]],
+            'messages_list': [], #list of message IDs
+            'start': 25,
+            'end': 75,
+    }]
     clear_v1()
