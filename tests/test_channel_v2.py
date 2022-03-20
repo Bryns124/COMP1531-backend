e13cd67409e4a276fcd801be83b2381c217fe6e1
z5359637 <z5359637@cse.unsw.edu.au>
2022-03-20 23:45:03 +1100

    added tests for channel_join

--- tests/test_channel_v2.py
+++ tests/test_channel_v2.py
@@ -31,7 +31,7 @@ def user_1():
 
 @pytest.fixture
 def user_2():
-    return  requests.post(f"{BASE_URL}/auth/register/v2", json={
+    return requests.post(f"{BASE_URL}/auth/register/v2", json={
         "email": "miguel@unsw.com",
         "password": "test123456",
         "name_first": "Miguel",
@@ -39,10 +39,9 @@ def user_2():
     })
 
 
-
 @pytest.fixture
 def user_no_access():
-    return  requests.post(f"{BASE_URL}/auth/register/v2", json={
+    return requests.post(f"{BASE_URL}/auth/register/v2", json={
         "email": "error@unsw.com",
         "password": "no_access1235667",
         "name_first": "no_access",
@@ -54,3 +53,106 @@ def user_no_access():
 def user_invalid():
     return jwt.encode({'auth_user_id': "invalid", 'session_id': 1}, SECRET, algorithm="HS256")
 
+
+@pytest.fixture
+def channel_public(user_1):
+    r = requests.post(f"{BASE_URL}/channels/create/v2", json={
+        "token": user_1['token'],
+        "name": "Test Channel",
+        "is_public": True
+    })
+    return r.json()
+
+
+@pytest.fixture
+def channel_private(user_1):
+    r = requests.post(f"{BASE_URL}/channels/create/v2", json={
+        "token": user_1['token'],
+        "name": "Private Channel",
+        "is_public": False
+    })
+    return r.json()
+
+
+@pytest.fixture
+def invalid_channel_id():
+    return -1
+
+
+def test_channel_details_input_error(user_1, invalid_channel_id):
+    r = requests.get(f"{BASE_URL}/channel/details/v2", json={
+        "token": user_1['token'],
+        "channel_id": invalid_channel_id
+    })
+    assert r.status_code == InputError.code
+
+
+def test_channel_details_access_error(user_2, channel_public):
+    r = requests.get(f"{BASE_URL}/channel/details/v2", json={
+        "token": user_2['token'],
+        "channel_id": channel_public
+    })
+    assert r.status_code == AccessError.code
+
+
+def test_channel_details_wrong_u_id(user_invalid, channel_public):
+    r = requests.get(f"{BASE_URL}/channel/details/v2", json={
+        "token": user_invalid['token'],
+        "channel_id": channel_public
+    })
+    assert r.status_code == AccessError.code
+
+
+def test_channel_details(user_1, channel_public):
+    r = requests.get(f"{BASE_URL}/channel/details/v2", json={
+        "token": user_1['token'],
+        "channel_id": channel_public
+    })
+    data = r.json()
+    assert data['channels'] == {
+        'name': "A New Hope",
+        'is_public': True,
+        'owner_members': [
+            {'u_id': 1, 'email': 'mikey@unsw.com', 'name_first': 'Mikey',
+                'name_last': 'Test', 'handle_str': 'mikeytest'}
+        ],
+        'all_members': [
+            {'u_id': 1, 'email': 'mikey@unsw.com', 'name_first': 'Mikey',
+                'name_last': 'Test', 'handle_str': 'mikeytest'}
+        ]
+    }
+
+
+def test_channel_details_multiple_users(user_1, channel_public, user_2):
+
+
+def test_channel_join_channel_id_error(user_1, invalid_channel_id):
+    r = requests.post(f"{BASE_URL}/channel/join/v2", json={
+        "token": user_1['token'],
+        "channel_id": invalid_channel_id
+    })
+    assert r.status_code == InputError.code
+
+
+def test_channel_join_already_member_error(user_1, channel_public):
+    r = requests.post(f"{BASE_URL}/channel/join/v2", json={
+        "token": user_1['token'],
+        "channel_id": channel_public
+    })
+    assert r.status_code == InputError.code
+
+
+def test_channel_access_error(user_2, channel_private):
+    r = requests.post(f"{BASE_URL}/channel/join/v2", json={
+        "token": user_2['token'],
+        "channel_id": channel_private
+    })
+    assert r.status_code == AccessError.code
+
+
+def test_channel_invalid_token(user_invalid, channel_public):
+    r = requests.post(f"{BASE_URL}/channel/join/v2", json={
+        "token": user_invalid['token'],
+        "channel_id": channel_public
+    })
+    assert r.status_code == AccessError.code
