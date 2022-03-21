4a6fd5c5ea841da16676a0645ec5ee707780f7ee
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-21 13:34:38 +1100

    Wrote the function dm_remove and also raised exceptions for the dm_create function

--- src/dm.py
+++ src/dm.py
@@ -24,6 +24,11 @@ def dm_create_v1(token, u_ids):
     if not auth_user_exist:
         raise AccessError
 
+    if check_duplicate(u_ids):
+        raise InputError
+
+    if check_invalid_id(store, u_ids):
+        raise InputError
 
     if store == {}:
         new_dm_id = 1
@@ -55,6 +60,21 @@ def dm_create_v1(token, u_ids):
         'DM_id': store['DM'][-1]['DM_id']
     }
 
+def check_duplicate(u_id_list):
+    ''' Check if given list contains any duplicates '''
+    if len(u_id_list) == len(set(u_id_list)):
+        return False
+    else:
+        return True
+
+def check_invalid_id(store, u_ids):
+    invalid = True
+    for ids in u_ids:
+        for user in store['users']:
+            if ids == user['u_id']:
+                invalid = False
+        if invalid == True:
+            return True
 
 def dm_list_v1(token):
     store = data_store.get()
@@ -75,7 +95,60 @@ def dm_list_v1(token):
             dm_list.append(extract_dm_details(store, dms['DM_id']))
 
 
-    def extract_dm_details(store, dm_id):
-        for dms in store['DM']:
-            if dms['DM_id'] == dm_id:
-                return dms
+def extract_dm_details(store, dm_id):
+    for dms in store['DM']:
+        if dms['DM_id'] == dm_id:
+            return dms
+
+
+def dm_remove_v1(token, dm_id):
+    store = data_store.get()
+    auth_user_exist = False
+    auth_user_id = decode_token(token)['auth_user_id']
+
+    for user in store['users']:
+        if auth_user_id == user['u_id']:
+            auth_user_exist = True
+
+    if not auth_user_exist:
+        raise AccessError
+
+    if not valid_dm_id(store, dm_id):
+        raise InputError
+
+    if not is_dm_owner(store, auth_user_id, dm_id):
+        raise AccessError
+
+    if not is_dm_member(store, auth_user_id, dm_id):
+        raise AccessError
+
+    count = 0
+    for dms in store['DM']:
+        if dms['DM_id'] == dm_id:
+            store['DM'].pop(count)
+        count += 1
+
+    return {
+
+    }
+
+def valid_dm_id(store, dm_id):
+    dm_exist = False
+    for dms in store['DM']:
+        if dm_id == dms['dm_id']:
+            dm_exist = True
+    return dm_exist
+
+def is_dm_owner(store, auth_user_id, dm_id):
+    dm_owner = False
+    for dms in store['DM']:
+        if dms['DM_id'] == dm_id and auth_user_id in dms['owner_members']:
+            dm_owner = True
+    return dm_owner
+
+def is_dm_member(store, auth_user_id, dm_id):
+    dm_member = False
+    for dms in store['DM']:
+        if dms['DM_id'] == dm_id and auth_user_id in dms['all_members']:
+            dm_member = True
+    return dm_member
