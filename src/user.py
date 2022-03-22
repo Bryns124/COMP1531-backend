ae2acc6d9553b826881a74a9a9b47b51ad752986
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-23 10:31:26 +1100

    Finished writing the functions for user.py

--- src/user.py
+++ src/user.py
@@ -2,6 +2,7 @@ from base64 import decode
 from src.data_store import data_store
 from src.error import InputError, AccessError
 from src.helper import decode_token, validate_token
+from src.auth import email_check, duplicate_email_check
 """User has the 5 functions: users_all, user_profile, user_profile_setname, user_profile_setemail, user_profile_sethandle
 Functions:
     users_all: Returns a list of all users and their associated details
@@ -31,4 +32,83 @@ def extract_user_details(user):
         'handle_str': user['handle_str']
         }
     return users
+
+
+def user_profile_v1(token, u_id):
+    validate_token(token)
+    store = data_store.get()
+    auth_user_id = decode_token(token)['auth_user_id']
+
+    for users in store['users']:
+        if users['u_id'] == u_id:
+            user = extract_user_details(users)
+
+    return user
+
+def user_profile_setname_v1(token, name_first, name_last):
+    validate_token(token)
+    store = data_store.get()
+    if not name_length_check(name_first):
+        raise InputError
+    if not name_length_check(name_last):
+        raise InputError
+
+    auth_user_id = decode_token(token)['auth_user_id']
+
+    for user in store['users']:
+        if user['u_id'] == auth_user_id:
+            user['name_first'] = name_first
+            user['name_last'] = name_last
+
+
+def user_profile_setemail_v1(token, email):
+    validate_token(token)
+    store = data_store.get()
+    if not valid_email(email):
+        raise InputError
+
+    auth_user_id = decode_token(token)['auth_user_id']
+
+    for user in store['users']:
+        if user['u_id'] == auth_user_id:
+            user['email'] = email
+
+
+def user_profile_sethandle_v1(token, handle_str):
+    validate_token(token)
+    store = data_store.get()
+    if not valid_handle_string(store, handle_str):
+        raise InputError
+
+    auth_user_id = decode_token(token)['auth_user_id']
+
+    for user in store['users']:
+        if user['u_id'] == auth_user_id:
+            user['handle_str'] = handle_str
+
+
+
+def name_length_check(name):
+    if any(len(name) > 50, len(name) < 1):
+        return False
+    else:
+        return True
+
+def valid_email(email):
+    if any(not email_check(email), duplicate_email_check(email)):
+        return False
+    else:
+        return True
+
+def valid_handle_string(store, handle_str):
+    if any(len(handle_str) > 20, len(handle_str) < 3, handle_str.isalnum(), duplicate_handle(store, handle_str)):
+        return False
+    else:
+        return True
+
+def duplicate_handle(store, handle_str):
+    for users in store['users']:
+        if handle_str == users['handle_str']:
+            return True
+    return False
 
