726e6a7c444e001f33150317e053decd75226945
Adiyat Rahman <z5346887@ad.unsw.edu.au>
2022-03-23 13:55:29 +1100

    Wrote docstrings for user.py

--- src/user.py
+++ src/user.py
@@ -13,6 +13,17 @@ Functions:
 """
 
 def users_all_v1(token):
+    """returns a list of all users and their associated details
+
+    Args:
+        token (string): user calling the function
+
+    Raises:
+        None
+
+    Returns:
+        dictionary: containing list of dictionaries with the details of the users
+    """
     validate_token(token)
     store = data_store.get()
 
@@ -36,6 +47,18 @@ def extract_user_details(user):
 
 
 def user_profile_v1(token, u_id):
+    """returns a the details of one particular user with the associated user_id
+
+    Args:
+        token (string): user calling the function
+        u_id (int): authorisation user id of the user whose details are required
+
+    Raises:
+        InputError: u_id does not refer to a valid user
+
+    Returns:
+        dictionary: containing the details of the user's profile
+    """
     validate_token(token)
     store = data_store.get()
     u_id = decode_token(token)['auth_user_id']
@@ -49,6 +72,17 @@ def user_profile_v1(token, u_id):
     }
 
 def user_profile_setname_v1(token, name_first, name_last):
+    """Updates the authorised user's first and last name
+    Args:
+        token (string): user who wants their name changed in their profile
+
+    Raises:
+        InputError: length of first name is more than 50 character or less than 1 character
+        InputError: length of first name is more than 50 character or less than 1 character
+
+    Returns:
+        None
+    """
     validate_token(token)
     store = data_store.get()
     if not name_length_check(name_first):
@@ -67,6 +101,17 @@ def user_profile_setname_v1(token, name_first, name_last):
 
 
 def user_profile_setemail_v1(token, email):
+    """Updates the authorised user's email
+    Args:
+        token (string): user who wants their email changed in their profile
+
+    Raises:
+        InputError: email does not follos valid email format
+        InputError: email is already in use by another user
+
+    Returns:
+        None
+    """
     validate_token(token)
     store = data_store.get()
     if not valid_email(email):
@@ -82,6 +127,18 @@ def user_profile_setemail_v1(token, email):
 
 
 def user_profile_sethandle_v1(token, handle_str):
+    """Updates the authorised user's handle name
+    Args:
+        token (string): user who wants their handle name changed in their profile
+
+    Raises:
+        InputError: length of handle name is not between 3 and 20 characters inclusive
+        InputError: handle_str contains characters that are not alphanumeric
+        InputError: the handle is already in use by another user
+
+    Returns:
+        None
+    """
     validate_token(token)
     store = data_store.get()
     if not valid_handle_string(store, handle_str):
@@ -102,24 +159,32 @@ def user_profile_sethandle_v1(token, handle_str):
 
 
 def name_length_check(name):
+    '''returns True if the length of the name is between 1 and 50 characters inclusive'''
     if any(len(name) > 50, len(name) < 1):
         return False
     else:
         return True
 
 def valid_email(email):
+    '''returns True if the email follows proper email formatting and is not already in use by another user'''
     if any(not email_check(email), duplicate_email_check(email)):
         return False
     else:
         return True
 
 def valid_handle_string(store, handle_str):
+    '''returns True if all three of the following things are satisfied:
+    - length is between 0 and 20 characters
+    - is alphanumeric
+    - not already in use
+    '''
     if any(len(handle_str) > 20, len(handle_str) < 3, handle_str.isalnum(), duplicate_handle(store, handle_str)):
         return False
     else:
         return True
 
 def duplicate_handle(store, handle_str):
+    '''returns True if the given handle string is already use'''
     for users in store['users']:
         if handle_str == users['handle_str']:
             return True
