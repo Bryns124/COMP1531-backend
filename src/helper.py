9d16076c13585cbd8b75f640d37159f5c79a55a1
Daniel <danielyung144@outlook.com>
2022-03-26 20:54:04 +1100

    added useful helper functions

--- src/helper.py
+++ src/helper.py
@@ -3,12 +3,20 @@ from src.error import AccessError, InputError
 from datetime import timezone
 import datetime
 import jwt
-
+import pickle
 
 SECRET = "ANT"
 
 
 def generate_token(u_id):
+    """
+    Takes a input user_id and generates a token for the user.
+    Args:
+        u_id (int): The users u_id
+
+    Returns:
+        string: A token which contains the users u_id and session_id
+    """
     valid_auth_user_id(u_id)
     store = data_store.get()
     for user in store['users']:
@@ -17,29 +25,47 @@ def generate_token(u_id):
                 len(user['session_id']) + 1)  # potential bug
     token = jwt.encode(
         {'auth_user_id': u_id, 'session_id': user['session_id'][-1]}, SECRET, algorithm="HS256")
+    data_store.set(store)
     return token
 
 
 def decode_token(token):
+    """
+    Takes in a users token and decodes it.
+    Args:
+        token (string): Users token.
+
+    Returns:
+        dict: Dictionary containing user's u_id and session_id.
+    """
     token_data = jwt.decode(token, SECRET, algorithms="HS256")
+    validate_token(token_data)
     return token_data
 
 
-def validate_token(token):
-    valid_auth_user_id(decode_token(token)['auth_user_id'])
+def validate_token(token_data):
+    """
+    Validates the users token information.
+    Args:
+        token_data (dict): Dictionary containing user's u_id and session_id
+
+    Raises:
+        AccessError: Raised when the session_id is invalid.
+    """
+    valid_auth_user_id(token_data['auth_user_id'])
     store = data_store.get()
     token_valid = False
     for user in store['users']:
         for session in user['session_id']:
-            if decode_token(token)['session_id'] == session:
+            if token_data['session_id'] == session:
                 token_valid = True
 
     if not token_valid:
-        raise AccessError("This token is invalid.")
+        raise AccessError(description="This token is invalid.")
 
 
 def valid_auth_user_id(auth_user_id):
-    """_summary_
+    """
     Validates that the input auth_user_id exists in the datastore
     Args:
         auth_user_id (u_id): The input u_id
@@ -56,11 +82,12 @@ def valid_auth_user_id(auth_user_id):
             auth_user_exist = True
 
     if not auth_user_exist:
-        raise AccessError("This auth_user_id does not exist in the datastore.")
+        raise AccessError(
+            description="This auth_user_id does not exist in the datastore.")
 
 
 def channel_validity(channel_id, store):
-    """_summary_
+    """
     Checks for a valid channel
     Args:
         channel_id (channel_id): _description_
@@ -75,8 +102,24 @@ def channel_validity(channel_id, store):
     return False
 
 
-def already_member(auth_user_id, channel_id, store):
+def user_validity(u_id, store):
     """_summary_
+    Checks for a valid channel
+    Args:
+        channel_id (channel_id): _description_
+        store (datastore): _description_
+
+    Returns:
+        _Boolean: Returns if the channel exists or not.
+    """
+    for users in store['users']:
+        if users['u_id'] == u_id:
+            return True
+    return False
+
+
+def already_member(auth_user_id, channel_id, store):
+    """
     Checks if a user is already a member of a channel
     Args:
         auth_user_id (u_id): The user id generated after auth login
@@ -95,14 +138,14 @@ def already_member(auth_user_id, channel_id, store):
 
 
 def extract_channel_details(channel_id, store):
-    """_summary_
+    """
     A method which coppies the data in the input_channel and returns it.
     Args:
-        channel_id (_type_): _description_
-        store (_type_): _description_
+        channel_id (int): ID of the channel info being looked for.
+        store (dict): The current state of datastore.
 
     Returns:
-        _type_: _description_
+        dict: the corresponding channel info
     """
     for channels in store['channels']:
         if channels['channel_id'] == channel_id:
@@ -111,7 +154,28 @@ def extract_channel_details(channel_id, store):
 
 
 def generate_timestamp():
+    """
+    Generates the times_sent for messages.
+    Returns:
+        int: Type cast int of the current utc timestamp.
+    """
     time = datetime.datetime.now(timezone.utc)
     utc = time.replace(tzinfo=timezone.utc)
     timestamp = utc.timestamp()
     return int(timestamp)
+
+
+def save_data_store():
+    """
+    Opens a file called datastore.p and saves the datastore within.
+    """
+    with open('datastore.p', 'wb') as FILE:
+        pickle.dump(data_store.get(), FILE)
+
+
+def load_data_store():
+    """
+    Opens a file called datastore.p and loads it into datstore.
+    """
+    with open('datastore.p', 'rb') as FILE:
+        data_store.set(pickle.load(FILE))
