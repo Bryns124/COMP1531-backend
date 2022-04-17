5f493860c44db57f331646d98aabe443fbd0aa0c
Michael Chai <michael.chai@student.unsw.edu.au>
2022-04-18 02:46:59 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 WORKING_DIRECTORY="~/www/cs1531deploy"
 
-USERNAME="TODO-TODO-TODO"
-SSH_HOST="TODO-TODO-TODO"
+USERNAME="m23"
+SSH_HOST="ssh-m23.alwaysdata.net"
 
 rm -rf ./**/__pycache__ ./**/.pytest_cache > /dev/null
 scp -r ./requirements.txt ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
