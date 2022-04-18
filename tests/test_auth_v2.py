7834935e65bd49d57198d9f92f4b024a8ae96a5e
Alice <z5362790@ad.unsw.edu.au>
2022-04-18 23:52:52 +1000

    imprved coverage

--- tests/test_auth_v2.py
+++ tests/test_auth_v2.py
@@ -355,3 +355,20 @@ def test_auth_passwordreset_email_sends():
     assert request_email.status_code == 200
 
     requests.delete(f"{BASE_URL}/clear/v1", json={})
+
+def test_auth_passwordreset_unregistered():
+    request_email = requests.post(f"{BASE_URL}/auth/passwordreset/request/v1", json={
+        "email": "danielyung144@outlook.com"
+    })
+    assert request_email.status_code == 200
+
+    requests.delete(f"{BASE_URL}/clear/v1", json={})
+
+def test_auth_passwordreset_unregistered():
+    request_email = requests.post(f"{BASE_URL}/auth/passwordreset/reset/v1", json={
+        "reset_code": "BLAH",
+        "new_password": "123456"
+    })
+    assert request_email.status_code == InputError.code
+
+    requests.delete(f"{BASE_URL}/clear/v1", json={})
