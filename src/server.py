0d71069dd07ce6ff9668034bc76992fc9bbfc1ba
Daniel <danielyung144@outlook.com>
2022-04-19 00:08:46 +1000

    fixing merge conflicts for auth_it3 into development3

--- src/server.py
+++ src/server.py
@@ -3,16 +3,13 @@ from json import dumps
 from flask import Flask, request
 from flask_mail import Mail, Message
 from flask_cors import CORS
-<<<<<<< HEAD
 from src.error import InputError
 from src import config, data_store
 from src.auth import auth_login_v1, auth_register_v1, auth_logout_v1, auth_passwordreset_request_v1, auth_passwordreset_reset_v1
 from src.message import messages_send_v1, message_senddm_v1, message_edit_v1, message_remove_v1
-=======
 from src import config
 from src.auth import auth_login_v1, auth_register_v1, auth_logout_v1
 from src.message import messages_send_v1, message_senddm_v1, message_edit_v1, message_remove_v1, search_v1, message_share_v1, message_sendlater_v1, message_sendlaterdm_v1, message_react_v1, message_unreact_v1, message_pin_v1, message_unpin_v1
->>>>>>> development3
 from src.channels import channels_list_v1, channels_listall_v1, channels_create_v1
 from src.channel import channel_details_v1, channel_join_v1, channel_invite_v1, channel_messages_v1, channel_leave_v1, channel_addowner_v1, channel_removeowner_v1
 from src.helper import save_data_store, load_data_store
