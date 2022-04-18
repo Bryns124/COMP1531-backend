59e74417d5f40e907e93d81c19ceb81513e40f65
Daniel <danielyung144@outlook.com>
2022-04-18 21:41:15 +1000

    added invalid token test for notif

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -128,7 +128,11 @@ def invalid_message_text():
     return "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Ne"
 
 
-def test_clear():
+def test_notifications_invalid_token(user_invalid):
+    request_notifications = requests.get(f"{BASE_URL}/notifications/get/v1", params={
+        "token": user_invalid
+    })
+    assert request_notifications.status_code == AccessError.code
     requests.delete(f"{BASE_URL}/clear/v1", json={})
 
 
